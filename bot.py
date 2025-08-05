eaf9b4ac90c904c862d06e02ca8b4db2fd8143ed
qsqcqs <qsqcqs@gmail.com>
2025-08-05 16:05:17 -0400

    1.0

--- bot.py
+++ bot.py
@@ -1,28 +1,70 @@
 import os
-import time
+import io #because someone else decided to use print command
+from contextlib import redirect_stdout
+
 import discord
-from dotenv import load_dotenv
+from dotenv import load_dotenv #so yall cant steal my discord
+import re #because stupid
 import datetime
-import id#id.py
+import shutil
+from id import id_from_cam#id.py
+import sys
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stderr
+        sys.stdout = self._stringio = io.StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stderr = self._stdout
 def day_month_year():
     return datetime.datetime.now().strftime("%d-%m-%Y")
-import sys
-crackdata=""
-if(sys.argv.__len__()%2==0):
-    for i in range(0,int(sys.argv.__len__()/2)-1):
-        
-        crackdata+=f"{sys.argv[2*i+1]}\n{sys.argv[2*i+2]}\n"
+
 #sorry, global variable
 load_dotenv("/home/qsqcqs/py/.env")
 TOKEN = os.getenv('DISCORD_TOKEN')
 #GUILD = os.getenv('DISCORD_GUILD')
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = io.StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
+
+
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-image=discord.File(os.getcwd()+"/"+sys.argv[-1]+"/image0.jpg")
-@client.event
-async def on_ready():
-    guild="need to do this bc python is dumb"
+
+
+async def take_pic_n_send():
+    try:
+        shutil.rmtree("runs")
+    except:
+        pass
+    with Capturing() as output:
+        id_from_cam()
+    val=str(output)
+
+    image=discord.File(os.getcwd()+"/runs/segment/predict/image0.jpg")
+    val=re.sub("\n"," ",val)
+    val=eval(val)
+    if val[0]=="no cracks lol":
+        return
+    crackdata=""
+    print(val[0])
+    if(val.__len__()%2==0):
+        
+        for i in range(0,int(val.__len__()/2)):
+            
+            crackdata+=f"{val[2*i]}\n{val[2*i+1]}\n"
+    print(crackdata)
+    guild="need to do this bc python is dumb about scope"
     for tmp_guild in client.guilds:
         if tmp_guild.name == "image dump":
             guild=tmp_guild
@@ -34,7 +76,12 @@ async def on_ready():
     if channel=="not defined yet":
         channel = await guild.create_text_channel(day_month_year())
     await channel.send(crackdata[:-1],file=image)
-    sys.exit(0)
+
+@client.event
+async def on_message(message):
+    if message.content==".cap":
+        await take_pic_n_send()
+
 
 #@client.event
 #async def on_message(message):
--- id.py
+++ id.py
@@ -5,49 +5,58 @@ from ultralytics import YOLO
 import os, sys
 import torch
 
+def id_from_cam(confidence_needed=.8):#80% arbitrarily
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
 
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-#image_path=sys.argv[1]
-if(sys.argv.__len__()>2): #second arg is confidence (optional)
-    confidence_needed=float(sys.argv[2])
-else:
-    confidence_needed=.8#80% arbitrarily
-#frame = cv2.imread(image_path)#ngl this is some random pic rn but cv2 works with camera imports
-model = YOLO("./YOLOv8-crack-seg/yolov8n/weights/best.pt") #general crack detection
-#yolov8n is also being used because its the more "lightweight" version and can hopefully run
-#without any internet stuff
-labels=["crack"] #its fortunately only crack and not detecting a crack
-results = model.predict(frame,verbose=False,conf=confidence_needed) #verbose means it talks, verbose=False means it shuts up
-crack_count=0
-for result in results:
-    for box in result.boxes:
-        print(f"{labels[int(box.cls)]}:{float(box.conf[0])*100}%")
-        if float(box.conf[0])>confidence_needed:
-            crack_count+=1
-oldmodel=model #debug thing, ignore this line
-
-model = YOLO("./YOLOv8-three-level-crack-classification/last.pt")  #classifying cracks
-labels=["Hairline","Structural Cracks","Spalling Cracks"] #ngl I just found these off the site docs
-
-
-confidence_needed_sub=confidence_needed
-outputs=[]
-confvals=[]
-while outputs.__len__()!=crack_count:
-    outputs=[]
-    confvals=[]
-    results = model.predict(frame,verbose=False,conf=confidence_needed_sub)
+#   debug:show image
+#    cv2.namedWindow("test")
+#    cv2.imshow("test",frame)
+#    k=cv2.waitKey(100)
+#   debug: use preset image
+    frame = cv2.imread("/home/qsqcqs/Videos/Untitled.jpg")#ngl this is some random pic rn but cv2 works with camera imports
+    model = YOLO("./YOLOv8-crack-seg/yolov8n/weights/best.pt") #general crack detection
+    #yolov8n is also being used because its the more "lightweight" version and can hopefully run
+    #without any internet stuff
+    labels=["Crack"] #its fortunately only crack and not detecting a crack
+    results = model.predict(frame,verbose=False,conf=confidence_needed) #verbose means it talks, verbose=False means it shuts up
+    crack_count=0
     for result in results:
         for box in result.boxes:
-            outputs.append([labels[int(box.cls)],float(box.conf[0])])
-            confvals.append(float(box.conf[0]))
-    confvals.sort(reverse=True)#descending order
-    if outputs.__len__()>crack_count: #tl;dr if too many cracks, increase standards so there arent
-        confidence_needed_sub=(confvals[crack_count]+confvals[crack_count-1])/2
-    if outputs.__len__()<crack_count: #tl;dr, if not enough cracks, lower standards so there are more
-        confidence_needed_sub=.3#idk .3 looks low enough, definitely an asspull of a number
-for thing in outputs:
-    print(f"{thing[0]}:{thing[1]*100}%")
-model.predict(frame,verbose=False,save=True,conf=confidence_needed_sub)
+            print(f"{labels[int(box.cls)]}:{float(box.conf[0])*100}%")
+            if float(box.conf[0])>confidence_needed:
+                crack_count+=1
+    oldmodel=model #debug thing, ignore this line
+    if crack_count==0:
+        results = model.predict(frame,save=True,verbose=False,conf=confidence_needed)
+
+        print("no cracks lol")
+        return(0)
+    model = YOLO("./YOLOv8-three-level-crack-classification/last.pt")  #classifying cracks
+    labels=["Hairline Crack","Structural Crack","Spalling Crack"] #ngl I just found these off the site docs
+
+    confidence_needed_sub=confidence_needed
+    outputs=[]
+    confvals=[]
+    while outputs.__len__()!=crack_count:
+        outputs=[]
+        confvals=[]
+        results = model.predict(frame,verbose=False,conf=confidence_needed_sub)
+        for result in results:
+            for box in result.boxes:
+                outputs.append([labels[int(box.cls)],float(box.conf[0])])
+                confvals.append(float(box.conf[0]))
+        confvals.sort(reverse=True)#descending order
+        if outputs.__len__()>crack_count: #tl;dr if too many cracks, increase standards so there arent
+            confidence_needed_sub=(confvals[crack_count]+confvals[crack_count-1])/2
+        if outputs.__len__()<crack_count: #tl;dr, if not enough cracks, lower standards so there are more
+            confidence_needed_sub=.3#idk .3 looks low enough, definitely an asspull of a number
+    for thing in outputs:
+        print(f"{thing[0]}:{thing[1]*100}%")
+
+    result2 = model.predict(frame,verbose=False,save=True,conf=confidence_needed_sub)
+
+    if result2  != results:
+        return 1
+    return 0
 
